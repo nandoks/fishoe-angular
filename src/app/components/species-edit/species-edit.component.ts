574e2c9d8b76e52b06ef5d4e03e9722c06ed14ab
nandoks <fernandooksman@gmail.com>
2026-07-26 21:57:54 +0200

    r9g edit page an add buttons

--- src/app/components/species-edit/species-edit.component.ts
+++ src/app/components/species-edit/species-edit.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
 @Component({
   selector: 'app-species-edit',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './species-edit.component.html',
   styleUrl: './species-edit.component.scss'
 })
